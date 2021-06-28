1f94020897ebd6ce67ddf31eb4f88ced1108882e
Paulo Vieira <paulovieira.hap@outlook.com>
2021-06-28 14:04:25 -0300

    Criando cobrinha

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let canvas = document.getElementById("Cobra");
 let context = canvas.getContext("2d");
-let box = 32
+let box = 32;
+
+let cobra = [];
+cobra[0] = {
+	x: 8 * box,
+	y: 8 * box
+}
 
 
 function criarBG(){
-	context.fillStyle = "black";
+	context.fillStyle = "lightgreen";
 	context.fillRect(0, 0, 16 * box, 16 * box)
+}
+
+function criarCobrinha (){
+	for(i=0; i<cobra.length; i++){
+		context.fillStyle = "green";
+		context.fillRect(cobra[i].x, cobra[i].y, 16 * box,  box)
+	}
 }
 
 criarBG
